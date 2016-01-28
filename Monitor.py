eace9f7300f56aec0fa9973aef21a294ff3037f1
iYonatan <yonatan.eilat@gmail.com>
2016-01-28 16:05:07 +0200

    Monitor.py has been added System.py - Network class on progress

---
+++ Monitor.py
@@ -0,0 +1,7 @@
+class Monitor(object):
+    def __init__(self):
+        self.first_packet = None
+        self.sec_pakcet = None
+
+    def Network_warning(self, packet):
+        pass
--- System.py
+++ System.py
@@ -7,7 +7,10 @@ import win32api
 import win32pdh
 import win32process
 
-from Config import *
+import socket
+import struct
+
+from functions import *
 from pywin32_Structs import *
 
 proc = ctypes.windll.Kernel32.OpenProcess(ALL_PROCESS_ACCESS, False, 2376)
@@ -17,7 +20,6 @@ proc = ctypes.windll.Kernel32.OpenProcess(ALL_PROCESS_ACCESS, False, 2376)
 # TODO: Done!
 
 class System:
-
     def __init__(self):
         pass
 
@@ -269,13 +271,132 @@ print d.disk_dict
 
 
 # ============================================================================ Network
-# TODO: Packet sniffer for: Ethernet, IP, TCP, ICMP, HTTP, FTP
+# TODO: Packet sniffer for: IP, TCP, UDP, ICMP, FTP
 
 
 class Network:
     def __init__(self):
         pass
 
+    def get_ipv4_addr(self, bytes_addr):
+        """
+        Returns a readble ipv4 from bytes (X.X.X.X)
+        :param bytes_addr: IP address in bytes
+        :return: readble ipv4 (string)
+        """
+        return socket.inet_ntoa(bytes_addr)
+
+    def IPv4_packet(self, data):
+        """
+        Extracts IPV4 packet
+        :param data: A packet (Bytes)
+        :return: version, ttl, protocol, src, target and the rest of the data
+        """
+        version_header_length, ttl, proto, src, target = struct.unpack('! B 7x B B 2x 4s 4s', data[:20])
+
+        version = version_header_length >> 4
+        header_length = (version_header_length & 15) * 4
+
+        return version, ttl, proto, self.get_ipv4_addr(src), self.get_ipv4_addr(target), data[20:]
+
+    def ICMP_packet(self, data):
+        """
+        Extracts ICMP packet
+        :param data: A packet (Bytes)
+        :return: icmp_type, code and checksum
+        """
+        icmp_type, code, checksum = struct.unpack('! B B H', data[:4])
+        return icmp_type, code, checksum, data[4:]
+
+    def TCP_segment(self, data):
+        """
+        Extracts TCP segment
+        :param data: A segment (Bytes)
+        :return: source port, destenation port, sequence number, acknowledgment number and all flags
+        """
+        (src_port, dest_port, sequence, ack, offset_reserved_flags) = struct.unpack('! H H L L H', data[:14])
+        offset = (offset_reserved_flags >> 12) * 4
+
+        flag_urg = (offset_reserved_flags & 32) >> 5
+        flag_ack = (offset_reserved_flags & 16) >> 4
+        flag_psh = (offset_reserved_flags & 8) >> 3
+        flag_rst = (offset_reserved_flags & 4) >> 2
+        flag_syn = (offset_reserved_flags & 2) >> 1
+        flag_fin = (offset_reserved_flags & 1)
+
+        return src_port, dest_port, sequence, ack, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[
+                                                                                                               offset:]
+
+    def UDP_segment(self, data):
+        """
+        Extracts UDP segment
+        :param data: A segment (Bytes)
+        :return: source port, destenation port, checksum and the rest data
+        """
+        (src_port, dest_port, checksum) = struct.unpack('! H H 2x H', data[:8])
+        return src_port, dest_port, checksum, data[8:]
+
+    def run(self):
+
+        TAB_1 = '\t - '
+        TAB_2 = '\t\t - '
+        TAB_3 = '\t\t\t - '
+
+        # TODO: Gets computer's ip through the config file
+
+        conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+        conn.bind(("10.92.5.59", 0))
+
+        # Include IP headers
+        conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+        # receive all packages
+        conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+        print "Continue...."
+
+        while True:
+            raw_data, addr = conn.recvfrom(65535)
+
+            version, ttl, proto, src, dest, data = self.IPv4_packet(raw_data)
+            print('\nIP Packet:')
+            print('Version: {}, Destination: {}, Source: {}, Next Protocol: {}'.format(version, dest, src, proto))
+
+            if proto == 1:
+                (icmp_type, code, checksum, data) = self.ICMP_packet(data)
+                print TAB_1 + "ICMP Packet:"
+                print (TAB_2 + 'icmp_type: {}, code: {}, checksum: {}'.format(icmp_type, code, checksum, data))
+
+            elif proto == 6:
+                (src_port, dest_port, sequence, ack,
+                 flag_urg,
+                 flag_ack,
+                 flag_psh,
+                 flag_rst,
+                 flag_syn,
+                 flag_fin,
+                 data) = self.TCP_segment(data)
+
+                print TAB_1 + "TCP segment:"
+                print (TAB_2 + 'src_port: {}, dest_port: {}'.format(src_port, dest_port, ))
+                print (
+                    TAB_3 + 'flag_urg: {}, flag_ack: {}, flag_psh: {},flag_rst: {}, flag_syn: {}, flag_fin: {}'.format(
+                            flag_urg,
+                            flag_ack,
+                            flag_psh,
+                            flag_rst,
+                            flag_syn,
+                            flag_fin))
+
+            elif proto == 17:
+                (src_port, dest_port, length, data) = self.UDP_segment(data)
+                print TAB_1 + "UDP segment:"
+                print (TAB_2 + 'Source Port: {}, Destination Port: {}, Length: {}'.format(src_port, dest_port, length))
+
+            else:
+                print(TAB_1 + 'Other IPv4 Data...')
+
+
 n = Network()
 print "# ============================================================================ # Network"
-# ============================================================================ Tests
+n.run()
